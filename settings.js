1bfea207eeb32902793416cd8ba7683c7f7ba99e
qateef34-cloud <qateef34@gmail.com>
2025-08-08 20:15:40 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,8 +2,8 @@ const settings = {
   packname: 'B͎E͎N͎Z͎I͎ C͎O͎M͎P͎A͎N͎Y͎ X͎M͎D͎',
   author: '‎',
   botName: "B͎E͎N͎Z͎ C͎O͎M͎P͎A͎N͎Y͎ X͎M͎D͎",
-  botOwner: '👑ALPHA-KING👑', // Your name
-  ownerNumber: '263718728504', //Set your number here without + symbol, just add country code & number without any space
+  botOwner: '👑QATEEF👑', // Your name
+  ownerNumber: '17077064559', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
